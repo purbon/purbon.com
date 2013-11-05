00c6eedc5700a3364cfc5aec1460f1a9bc833ffd
Pere Urbon-Bayes <pere.urbon@belectric.com>
2013-11-05 16:34:16 +0100

    first import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PurbonCom::Application.config.secret_key_base = '2c0ef36e2075ed49688cb4fef935f2ab729834ccf33fa40c54e9570a7de0beb2b1d1d6680510d12c943a8dfbc6e7cfe4f679c17025ce6fbf0b3c2c24be57b276'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PurbonCom::Application.config.session_store :cookie_store, key: '_purbon_com_session'
