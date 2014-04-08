b291b98c3d5a4d75379fdf0a700d6efb7d4be078
Pere Urbon-Bayes <pere.urbon@gmail.com>
2014-04-08 14:31:35 +0200

    fix configuration variables

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,21 @@ module PurbonCom
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    APP_CONFIG = YAML.load_file("#{Rails.root}/config/env.yml")[Rails.env]
+
     config.i18n.default_locale = :en
+    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            APP_CONFIG['username'],
+      password:             APP_CONFIG['password'],
+      authentication:       'plain',
+      enable_starttls_auto: true  
+    }
+
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,18 +27,4 @@ PurbonCom::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            'pere.urbon@gmail.com',
-    password:             './VooDoo./',
-    authentication:       'plain',
-    enable_starttls_auto: true  
-  }
-  
-
 end
