d8c4f1a7b4609753bf1e4b08d5e6ea624cc1cf9b
Pere Urbón <purbon@users.noreply.github.com>
2016-11-27 17:00:51 +0100

    Feature/update (#1)
    * update to Rails 5, remove of foundation and make use of skeleton for css design, upated formatting of the pages
    
    * style cv section
    
    * update git ignore
    
    * update cv and header style
    
    * updated styles
    
    * initial setup of an slides and conferences page
    
    * reformat of left side bar
    
    * update services page
    
    * style proper menu
    
    * rename principal for services
    
    * fix wrong wording
    
    * formatting fix
    
    * ammend blockers for the catalan version
    
    * ammend routes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-PurbonCom::Application.config.session_store :cookie_store, key: '_purbon_com_session'
+Rails.application.config.session_store :cookie_store, key: '_purbon_com_session'
